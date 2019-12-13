0398c49d9ea585ca5d201ea6443632770a571ab9
Shunya <yamasyunizig1216@gmail.com>
2019-12-13 18:42:15 +0900

    Messageをfirebaseに保存できるようにした。

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -9,27 +9,48 @@
           >
           <v-btn type="submit"></v-btn>
           </v-text-field>
+          <h5 class="red--text text--lighten-2" v-if="feedback" >{{feedback}}</h5>
           </form>
    </v-container>
 </template>
 
+
+// スクリプト
 <script>
+import db from '@/firebase/init'
+
 export default {
    props: ['name'],
    data(){
       return{
          newMessage: null,
+         feedback: null,
       }
    },
    methods: {
+      // firebaseに送る
       submittext(){
-         console.log(this.newMessage, this.name, Date.now())
+         if(this.newMessage){
+            db.collection('messages').add({
+               content: this.newMessage,
+               name: this.name,
+               timestamp: Date.now()
+            }).catch(err => {
+               console.log(err)
+            })
+            // ここでフォームを空にしている。
+            // これが無いとMEssageが残ったままになってしまう。
+            this.newMessage = null
+            this.feedback = null
+         } else {
+            this.feedback = "You must enter something"
+         }
       }
    }
 }
 </script>
 
-
+// スタイル
 <style scoped>
 
 </style>
