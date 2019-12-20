1abe13ebf3125233dcfec8ab0b33acc8ee422881
Shunya <yamasyunizig1216@gmail.com>
2019-12-21 00:23:08 +0900

    名前重複確認機能 追加

--- src/components/user/Signup.vue
+++ src/components/user/Signup.vue
@@ -11,6 +11,7 @@
                <h2 class="title">Signup</h2>
 
                <form @submit.prevent="signup">
+                  <p red--text text--lighten-1 center v-if="feedback">{{ feedback }}</p>
                   <v-text-field label="Email" name="email" required v-model="email">
                   </v-text-field>
                   <v-text-field label="Password" name="password" required v-model="password">
@@ -31,13 +32,18 @@
 </template>
 
 <script>
+import slugify from 'slugify'
+import db from '@/firebase/init'
+
 export default {
    data(){
       return {
          email: null,
          password: null,
          name: null,
-         imageUrl: 'https://image.freepik.com/free-vector/social-media-refer-friend-concept_23-2148260460.jpg'
+         feedback: null,
+         slug: null,
+         imageUrl: 'https://image.freepik.com/free-vector/social-media-refer-friend-concept_23-2148260460.jpg',
       }
    },
    computed: {
@@ -48,8 +54,31 @@ export default {
       }
    },
    methods: {
+   // User登録
       signup(){
-         console.log(this.name)
+         // slugをユーザーの認識子として使用する。
+         if(this.name){
+            this.slug = slugify(this.name, {
+               replacement: '-',
+               remove: /[$*_+~.()'"!\-:@]/g,
+               lower: true
+            })
+
+            // User内に同名の名前があるか確認。
+            let ref = db.collection('users').doc(this.slug)
+            ref.get().then(doc => {
+               // 重複した場合
+               if(doc.exists){
+                  this.feedback = "This Username already exists"
+               } else {
+               // 使える場合
+                  this.feedback = "This username is free to use"
+               }
+            })
+            console.log(this.slug)
+         } else {
+            this.feedback = "You must enter an User name "
+         }
       }
    }
 }
@@ -78,3 +107,5 @@ form {
      margin-bottom: 30px
   }
 </style>
+
+// Usernameを認識子として利用したい
